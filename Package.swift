864fc14e2ce656bdf6c2feda2c9bc74a0e4bca63
Vitalii Parovishnyk <korich.vi.p@gmail.com>
2020-11-04 09:32:29 +0200

    fixed compiler warning for xcode 12

--- Package.swift
+++ Package.swift
@@ -6,9 +6,6 @@ import PackageDescription
 
 let package = Package(
     name: "M13Checkbox",
-    platforms: [
-        .iOS(.v8)
-    ],
     products: [
         .library(name: "M13Checkbox", targets: ["M13Checkbox"]),
     ],
